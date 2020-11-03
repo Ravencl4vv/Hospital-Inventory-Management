43e0ade3523ae094399962c0c622b1b7dab1db2c
Ravencl4vv <sebastian.rv.2001@gmail.com>
2020-11-02 23:46:29 -0500

    Terminando User Storie List-Medical-Equipment

--- src/main/java/pe/edu/pe/Hospital/Inventory/Management/controllers/MedicalEquipmentController.java
+++ src/main/java/pe/edu/pe/Hospital/Inventory/Management/controllers/MedicalEquipmentController.java
@@ -10,18 +10,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pe.edu.pe.Hospital.Inventory.Management.entities.MedicalEquipment;
+import pe.edu.pe.Hospital.Inventory.Management.entities.Model;
 import pe.edu.pe.Hospital.Inventory.Management.services.MedicalEquipmentService;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/medical-equipment")
+@RequestMapping("")
 public class MedicalEquipmentController {
     @Autowired
     private MedicalEquipmentService medicalEquipmentService;
 
 
-    @GetMapping(path = "/id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/medical-equipments/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<MedicalEquipment> fetchById(@PathVariable("id") Integer id){
         try{
             Optional<MedicalEquipment> optionalMedicalEquipment = medicalEquipmentService.findById(id);
@@ -35,6 +37,20 @@ public class MedicalEquipmentController {
         catch (Exception e){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+    @GetMapping("/medical-equipments")
+    public ResponseEntity<List<MedicalEquipment>> findAll() {
+        try {
+            List<MedicalEquipment> medicalEquipments = medicalEquipmentService.findAll();
+            if (medicalEquipments.isEmpty()) {
+                return  ResponseEntity.noContent().build();
+            } else{
+                return  ResponseEntity.ok(medicalEquipments);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
 }
