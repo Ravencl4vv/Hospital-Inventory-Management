1e7dc998b75cb28a59a01252a4c2966eb40fd450
Ravencl4vv <sebastian.rv.2001@gmail.com>
2020-11-02 23:37:39 -0500

    Terminado el User Storie Delete

--- src/main/java/pe/edu/pe/Hospital/Inventory/Management/controllers/MedicalEquipmentController.java
+++ src/main/java/pe/edu/pe/Hospital/Inventory/Management/controllers/MedicalEquipmentController.java
@@ -5,23 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pe.edu.pe.Hospital.Inventory.Management.entities.MedicalEquipment;
+import pe.edu.pe.Hospital.Inventory.Management.entities.Model;
 import pe.edu.pe.Hospital.Inventory.Management.services.MedicalEquipmentService;
 
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/medical-equipment")
+@RequestMapping("")
 public class MedicalEquipmentController {
     @Autowired
     private MedicalEquipmentService medicalEquipmentService;
 
 
-    @GetMapping(path = "/id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/medical-equipment/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<MedicalEquipment> fetchById(@PathVariable("id") Integer id){
         try{
             Optional<MedicalEquipment> optionalMedicalEquipment = medicalEquipmentService.findById(id);
@@ -36,5 +34,17 @@ public class MedicalEquipmentController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @DeleteMapping("/medical-equipment/{id}")
+    public ResponseEntity<MedicalEquipment> deleteById(@PathVariable(name = "id") Integer id) {
+
+        try {
+            return (ResponseEntity<MedicalEquipment>) medicalEquipmentService.deleteById(id);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 
 }
